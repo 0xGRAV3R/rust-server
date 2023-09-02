40f8c35f91d7b3f24a01ff4a084482430cd57544
0xGRAV3R <ycopyart.xyz@gmail.com>
2023-09-02 08:42:42 -0700

    🔸 added 404.html page

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,20 @@ fn handle_connection(mut stream: TcpStream) {
     
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
-        
+
     } else {
+
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+    
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+    
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
 
     }
 
