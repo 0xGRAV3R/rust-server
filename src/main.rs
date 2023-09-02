8c26fea3d1c68251edaa76a4990351da438496fc
0xGRAV3R <ycopyart.xyz@gmail.com>
2023-09-02 08:48:12 -0700

    🔸 refactor code using tuple

--- src/main.rs
+++ src/main.rs
@@ -21,35 +21,26 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with( get) {
+    let (status_line, filename) = 
+        if buffer.starts_with( get) {
+            ("HTTP/1.1 404 NOT FOUND", "index.html")
 
-        let contents = fs::read_to_string("index.html").unwrap();
-    
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-    
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+  
 
-    } else {
+        };
 
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-    
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-    
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
 
-    }
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
     
     
 
