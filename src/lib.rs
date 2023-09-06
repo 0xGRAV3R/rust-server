580a90e4adf9728012c73bf764b0f00b0f47a0e0
0xGRAV3R <ycopyart.xyz@gmail.com>
2023-09-06 15:09:47 -0700

    🧵 Added struct Worker

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@ use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -15,14 +18,16 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel
+
         let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
+        for id in 0..size {
             // create threads
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, receiver));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
     
     pub fn execute<F>(&self, f: F)
