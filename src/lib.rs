4f5fe95f0b2a8443fd0682c3802c5bbbb2cb6e5a
0xGRAV3R <ycopyart.xyz@gmail.com>
2023-09-08 17:12:19 -0700

    👋 Graceful Shutdown and Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::{sync::{Arc, Mutex, mpsc}, thread};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -44,12 +44,14 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
@@ -68,13 +70,25 @@ struct Worker {
 
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver
+            let message = receiver
                 .lock()
                 .unwrap()
                 .recv()
                 .unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();               
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+
+            }
 
             
             println!("Worker {} got a job; executing.", id);
