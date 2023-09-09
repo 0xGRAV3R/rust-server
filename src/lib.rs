99cc2e3c51748dbfe260885a07a5ad84b43acf1b
0xGRAV3R <ycopyart.xyz@gmail.com>
2023-09-08 17:13:39 -0700

    👋 Graceful Shutdown and Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,10 @@ impl Drop for ThreadPool {
     fn drop(&mut self) {
         println!("Sending terminate message to all workers.");
 
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
